b285d5eae06fdd25432747400fcdf30f1f533fbf
Ievgen Golovachov <ievgen.golovachov@idealscorp.com>
2025-02-06 22:32:07 +0200

    feat: setup metadata information only for root route

--- backend/src/middlewares/metadata.middleware.js
+++ backend/src/middlewares/metadata.middleware.js
@@ -1,7 +1,10 @@
-const metadataMiddleware = (_, res, next) => {
-  res.setHeader('X-App-CreatedBy', 'gappbox');
-  res.setHeader('X-App-Name', 'mockly');
-  res.setHeader('X-App-Version', require('../../package.json').version || 'unknown');
+const metadataMiddleware = (req, res, next) => {
+  if (req.originalUrl === '/') {
+    res.setHeader('X-App-CreatedBy', 'gappbox');
+    res.setHeader('X-App-Name', 'mockly');
+    res.setHeader('X-App-Version', require('../../package.json').version || 'unknown');
+  }
+
   next();
 };
 
